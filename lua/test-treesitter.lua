27d841210fab8b6e31dbf046d184584024a702bb
James Newman <jdnewman85@gmail.com>
2022-12-23 20:52:12 -0700

    Tests with treesitter, bpe, etc

---
+++ lua/test-treesitter.lua
@@ -0,0 +1,34 @@
+local ts = vim.treesitter
+local ts_utils = require 'nvim-treesitter.ts_utils'
+
+local function_node_names={
+  'function_declaration',
+  'function_definition',
+  'local_function',
+  'method_definition',
+  'method_declaration',
+  'constructor_declaration',
+}
+
+function matchesAny(input, matches)
+  for _, match in ipairs(matches) do
+    if string.match(input, match) then
+      return true
+    end
+  end
+  return false
+end
+
+function select_function_at_cursor()
+  local bufnr = 0
+  local parser = ts.get_parser(bufnr)
+  local tree = parser:parse()
+  local node = ts_utils.get_node_at_cursor()
+  while node and not matchesAny(node:type(), function_node_names) do
+    node = node:parent()
+  end
+  if not node then return end
+  ts_utils.update_selection(bufnr, node, 'V')
+end
+
+vim.api.nvim_set_keymap('n', '<leader><Space>', ':lua select_function_at_cursor()<CR>', { noremap = true, silent = false })
--- lua/test2.lua
+++ lua/test2.lua
@@ -1,7 +1,7 @@
 local ts_utils = require("nvim-treesitter.ts_utils")
 local menus = require('openai_menus')
 
-
+--[[
 local function get_top_node_under_cursor()
   local cursor_node = ts_utils.get_node_at_cursor()
   if cursor_node == nil then
@@ -16,7 +16,38 @@ end
 local node = get_top_node_under_cursor()
 local bufnr = vim.api.nvim_get_current_buf()
 ts_utils.update_selection(bufnr, node)
-print"HI"
+]]--
 
 
 --menus.show_menu()
+
+local ts = vim.treesitter
+local ts_utils = require 'nvim-treesitter.ts_utils'
+
+local query = [[
+    (function_declaration) @matchMe
+]]
+local parsed_query = ts.parse_query("lua", query)
+
+function test_get_node()
+    local bufnr = 0
+    local start_node = ts_utils.get_node_at_cursor()
+    local parser = ts.get_parser(bufnr, "lua")
+    local root = parser:parse()[1]:root()
+    local start_row, _, end_row, _ = root:range()
+
+    print_node("Node at cursor", start_node)
+    print("sexpr: " .. start_node:sexpr())
+
+    for id, node in parsed_query:iter_captures(start_node, bufnr, start_row, end_row) do
+        local name = parsed_query.captures[id] -- name of the capture in the query
+        print("- capture name: " .. name)
+        print_node(string.format("- capture node id(%s)", id), node)
+    end
+end
+
+function print_node(title, node)
+    print(string.format("%s: type '%s' isNamed '%s'", title, node:type(), node:named()))
+end
+
+vim.api.nvim_set_keymap('n', '<leader><Space>', ':lua test_get_node()<CR>', { noremap = true, silent = false })
