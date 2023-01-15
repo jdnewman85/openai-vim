0dac5d3ad8ec444e5749bc0721cfb254ef01628d
James Newman <jdnewman85@gmail.com>
2023-01-15 07:08:17 -0700

    Tidy. Change to operate on current buffer selection

--- lua/openai.lua
+++ lua/openai.lua
@@ -3,79 +3,64 @@ local models = require('openai_models')
 
 local M = {}
 
-function M.open_window()
-  new_buf = vim.api.nvim_create_buf(true, true)
-  vim.api.nvim_buf_set_option(new_buf, 'bufhidden', 'wipe')
-
-  local term_width = vim.api.nvim_get_option('columns')
-  local term_height = vim.api.nvim_get_option('lines')
-
-  local win_width = math.ceil(term_width * 0.7)
-  local win_height = math.ceil(term_height * 0.5 - 4)
-
-  local row = math.ceil((term_height - win_height) / 2 - 1)
-  local col = math.ceil((term_width - win_width) / 2)
-
-  local opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width,
-    height = win_height,
-    row = row,
-    col = col
-  }
-
-  local focus_new_window = false
-  local new_win = vim.api.nvim_open_win(new_buf, focus_new_window, opts)
-
-  return new_win, new_buf
-end
-
 --[[
 --TODO
 If stream == true, and no callback, use function append to target location?
 ]]--
-function M.openai_request(endpoint, data)
-  local openai_url = "https://api.openai.com/v1/"
+function M.request(endpoint, data, on_stdout)
+  local url = "https://api.openai.com/v1/"
   local curl = require "plenary.curl"
   local auth_token = vim.env["OPENAI_APIKEY"]
 
   local headers = {
+    ["Authorization"] = "Bearer " .. auth_token,
     ["Content-Type"] = "application/json",
-    ["Authorization"] = "Bearer " .. auth_token
   }
 
-  local _, ai_buf = M.open_window()
 
-  local job = curl.post (openai_url .. endpoint, {
+  local curl_opts = {
     body = vim.fn.json_encode(data),
     headers = headers,
-    on_stdout = M._create_append_to_buffer_func(ai_buf),
-  })
-  --[[
-  local response = curl.post (openai_url .. endpoint, {
-    body = vim.fn.json_encode(data),
-    headers = headers,
-    on_stdout = M.print_things,
-  }).body
-  local response_decoded = vim.fn.json_decode(response)
+  }
+
+  local stream_opt = data["stream"]
+  if stream_opt and not on_stdout then
+    local current_buffer = 0
+    local _, _, insert_row, insert_column = utils.visual_selection_range()
+    if insert_column == 2147483647 then
+      insert_row = insert_row + 1
+      insert_column = 0
+    end
+    on_stdout = M._create_append_to_buffer_func(current_buffer, insert_row+1, insert_column)
+  end
+
+  if on_stdout then
+    curl_opts["on_stdout"] = on_stdout
+  end
+  local job_or_response = curl.post(url .. endpoint, curl_opts)
+  if stream_opt then
+    job = job_or_response
+    return job
+  end
+
+  local response = job_or_response
+  local response_decoded = vim.fn.json_decode(response.body)
 
   return response_decoded
-  ]]--
-  return "TEMPORARY\n"
 end
 
-function M.openai_edits()
+function M.edits()
   local data = {
     model = "text-davinci-edit-001",
     input = utils.buf_vtext(),
     instruction = "Fix the spelling mistakes"
   }
-  local response_decoded = M.openai_request("edits", data)
+  local response_decoded = M.request("edits", data)
 --  vim.pretty_print(response_decoded)
+  return response_decoded
 end
 
-function M.openai_completion()
+function M.complete_selection()
   local txt = utils.buf_vtext()
   local data = {
     model = "code-davinci-002",
@@ -84,11 +69,15 @@ function M.openai_completion()
     temperature = 0,
     stream = true,
   }
-  local response_decoded = M.openai_request("completions", data)
+  local response_decoded = M.request("completions", data)
 --  vim.pretty_print(response_decoded)
+  return response_decoded
 end
 
-function M._create_append_to_buffer_func(target_buffer)
+function M._create_append_to_buffer_func(target_buffer, start_insert_row, start_insert_column)
+  local insert_row = start_insert_row-1
+  local insert_column = start_insert_column
+
   local fn =  function(err, data, job)
     -- Trim off `Data :`
     local maybe_json = string.sub(data, 7, -1)
@@ -106,12 +95,18 @@ function M._create_append_to_buffer_func(target_buffer)
     vim.schedule(function()
       local d = vim.fn.json_decode(maybe_json)
       local resp = d.choices[1].text
-      local end_of_buf = utils.buf_get_end_pos(target_buffer)
-      local insert_row = end_of_buf[1]-1
-      local insert_col = end_of_buf[2]
 
       local resp_array = utils.string_split(resp, "\n")
-      vim.api.nvim_buf_set_text(target_buffer, insert_row, insert_col, insert_row, insert_col, resp_array)
+      vim.api.nvim_buf_set_text(target_buffer, insert_row, insert_column, insert_row, insert_column, resp_array)
+
+      local num_inserted_rows = #resp_array-1
+      local response_last_line_length = string.len(resp_array[#resp_array])
+      insert_row = insert_row + num_inserted_rows
+      if num_inserted_rows > 0 then
+        insert_column = response_last_line_length
+      else
+        insert_column = insert_column + response_last_line_length
+      end
     end)
   end
 
