f641fa85832166b0179c0a407f95c83b6532ef03
James Newman <jdnewman85@gmail.com>
2023-01-22 22:05:10 -0700

    Tidy

--- lua/functional.lua
+++ lua/functional.lua
@@ -10,29 +10,9 @@ function M.filter(collection, predicate)
   return result
 end
 
--- ipairs/array version
-function M.filter_i(collection, predicate)
-  local result = {}
-  for _, v in ipairs(collection) do
-    if predicate(v) then
-      table.insert(result, v)
-    end
-  end
-  return result
-end
-
 function M.map(collection, transform)
   local result = {}
   for _, v in pairs(collection) do
-    table.insert(result, transform(v))
-  end
-  return result
-end
-
--- ipairs/array version
-function M.map_i(collection, transform)
-  local result = {}
-  for _, v in ipairs(collection) do
     table.insert(result, transform(v))
   end
   return result
@@ -46,26 +26,8 @@ function M.reduce(collection, initial, reducer)
   return result
 end
 
--- ipairs/array version
-function M.reduce_i(collection, initial, reducer)
-  local result = initial
-  for _, v in ipairs(collection) do
-    result = reducer(result, v)
-  end
-  return result
-end
-
 function M.find(collection, predicate)
   for _, v in pairs(collection) do
-    if predicate(v) then
-      return v
-    end
-  end
-end
-
--- ipairs/array version
-function M.find_i(collection, predicate)
-  for _, v in ipairs(collection) do
     if predicate(v) then
       return v
     end
@@ -75,7 +37,7 @@ end
 function M.contains(collection, item)
   for _, v in pairs(collection) do
     if v == item then
-      return v
+      return v --TODO Maybe it would be more useful to return the key?
     end
   end
   return false
@@ -87,6 +49,50 @@ function M.keys(collection)
     table.insert(result, k)
   end
   return result
+end
+
+-- ipairs/array versions
+function M.filter_i(collection, predicate)
+  local result = {}
+  for _, v in ipairs(collection) do
+    if predicate(v) then
+      table.insert(result, v)
+    end
+  end
+  return result
+end
+
+function M.map_i(collection, transform)
+  local result = {}
+  for _, v in ipairs(collection) do
+    table.insert(result, transform(v))
+  end
+  return result
+end
+
+function M.reduce_i(collection, initial, reducer)
+  local result = initial
+  for _, v in ipairs(collection) do
+    result = reducer(result, v)
+  end
+  return result
+end
+
+function M.find_i(collection, predicate)
+  for _, v in ipairs(collection) do
+    if predicate(v) then
+      return v
+    end
+  end
+end
+
+function M.contains_i(collection, item)
+  for _, v in ipairs(collection) do
+    if v == item then
+      return v --TODO Maybe it would be more useful to return the key?
+    end
+  end
+  return false
 end
 
 return M
