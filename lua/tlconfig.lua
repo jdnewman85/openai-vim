43ae6b91986879a5555152210ba16d39e622dd8b
James Newman <jdnewman85@gmail.com>
2023-02-05 17:08:33 -0700

    Restructure directory layout

--- lua/tlconfig.lua
+++
@@ -1,7 +0,0 @@
-return {
-  source_dir = "teal-src",
-  build_dir = "out",
-  global_env_def = "external/vim",
-  include_dir = {"teal-src", "external"},
-  gen_compat = "off",
-}
---
+++ tlconfig.lua
@@ -0,0 +1,7 @@
+return {
+  source_dir = "teal",
+  build_dir = "lua",
+  global_env_def = "external/vim",
+  include_dir = {"teal", "external"},
+  gen_compat = "off",
+}
